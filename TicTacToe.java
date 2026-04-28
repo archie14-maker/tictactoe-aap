deafcd044a559c9e33963db6b7b116949c2df2f0
Aaradhya Archie <aaradhyaarchiee@gmail.com>
2026-04-28 10:53:10 +0530

    UC7: Computer random valid move implemted

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,19 +1,56 @@
+import java.util.Random;
 public class TicTacToe {
 
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+
+    static char computerSymbol = 'O';
     public static void main(String[] args) {
-        char[][] board = new char[3][3];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = '-';
+        computerMove();
+        printBoard();
+    }
+    static void computerMove() {
+        Random rand = new Random();
+        int slot;
+        int row, col;
+
+        while (true) {
+            slot = rand.nextInt(9) + 1;
+            row = getRowFromSlot(slot);
+            col = getColFromSlot(slot);
+            if (isValidMove(row, col)) {
+                placeMove(row, col, computerSymbol);
+                System.out.println("Computer chose slot: " + slot);
+                break;
             }
         }
-        System.out.println("Tic-Tac-Toe Board:");
+    }
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
+
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
+    }
+    static boolean isValidMove(int row, int col) {
+        return row >= 0 && row < 3 &&
+               col >= 0 && col < 3 &&
+               board[row][col] == '-';
+    }
+
+    static void placeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
+    }
+    static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j] + " ");
             }
-            System.out.println(); // new line after each row
+            System.out.println();
         }
     }
 }
