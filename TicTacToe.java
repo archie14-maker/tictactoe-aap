802957a35f7684118c2ad46c5fcc5dbcae2e2c74
Aaradhya Archie <aaradhyaarchiee@gmail.com>
2026-04-28 10:51:24 +0530

    UC7: Computer random valid move implemted

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,25 +1,56 @@
-public class TicTacToeUC6 {
+import java.util.Random;
+public class TicTacToe {
 
-    static char[][] board = new char[3][3];
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
 
+    static char computerSymbol = 'O';
     public static void main(String[] args) {
-
-        initializeBoard();
-
-        placeMove(0, 0, 'X');
-
-        System.out.println("Cell (0,0): " + board[0][0]);
+        computerMove();
+        printBoard();
     }
+    static void computerMove() {
+        Random rand = new Random();
+        int slot;
+        int row, col;
 
-    static void initializeBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
+        while (true) {
+            slot = rand.nextInt(9) + 1;
+            row = getRowFromSlot(slot);
+            col = getColFromSlot(slot);
+            if (isValidMove(row, col)) {
+                placeMove(row, col, computerSymbol);
+                System.out.println("Computer chose slot: " + slot);
+                break;
             }
         }
     }
 
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
+
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
+    }
+    static boolean isValidMove(int row, int col) {
+        return row >= 0 && row < 3 &&
+               col >= 0 && col < 3 &&
+               board[row][col] == '-';
+    }
+
     static void placeMove(int row, int col, char symbol) {
         board[row][col] = symbol;
+    }
+    static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
