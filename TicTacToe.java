41ead41507b69745a996110dbc926dbbcb230e12
Aaradhya Archie <aaradhyaarchiee@gmail.com>
2026-04-28 10:50:34 +0530

    UC6: Place move on board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,19 +1,25 @@
-public class TicTacToe {
+public class TicTacToeUC6 {
+
+    static char[][] board = new char[3][3];
 
     public static void main(String[] args) {
-        char[][] board = new char[3][3];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
+
+        initializeBoard();
+
+        placeMove(0, 0, 'X');
+
+        System.out.println("Cell (0,0): " + board[0][0]);
+    }
+
+    static void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 board[i][j] = '-';
             }
         }
-        System.out.println("Tic-Tac-Toe Board:");
+    }
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println(); // new line after each row
-        }
+    static void placeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
     }
 }
