048dd53c6cc5d7bf8a5623aff3683d9081fec076
Aaradhya Archie <aaradhyaarchiee@gmail.com>
2026-04-28 10:48:36 +0530

    UC3: Accept user slot input (1-9)
    Refactored TicTacToe class to TicTacToeUC3 and added user input for slot selection.

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,19 +1,14 @@
-public class TicTacToe {
+import java.util.Scanner;
 
+public class TicTacToeUC3 {
     public static void main(String[] args) {
-        char[][] board = new char[3][3];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = '-';
-            }
-        }
-        System.out.println("Tic-Tac-Toe Board:");
-
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println(); // new line after each row
-        }
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
+    }
+    static int getUserSlot() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter slot (1-9): ");
+        int slot = sc.nextInt();
+        return slot;
     }
 }
