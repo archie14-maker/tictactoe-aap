4614c9236ad65b3b7c9eab8dee93ee8046997ba7
Aaradhya Archie <aaradhyaarchiee@gmail.com>
2026-04-28 10:56:06 +0530

    UC9: Implement win checking logic

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,22 +1,33 @@
 public class TicTacToe {
 
     static char[][] board = {
-        {'X', 'O', 'X'},
-        {'X', 'O', 'O'},
-        {'O', 'X', 'X'}
+        {'X', 'X', 'X'},
+        {'-', 'O', '-'},
+        {'O', '-', '-'}
     };
 
     public static void main(String[] args) {
-        System.out.println(isDraw());
+        System.out.println(hasWon('X'));
     }
 
-    static boolean isDraw() {
-        for (int r = 0; r < 3; r++) {
-            for (int c = 0; c < 3; c++) {
-                if (board[r][c] == '-')
-                    return false;
-            }
+    static boolean hasWon(char symbol) {
+
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol)
+                return true;
         }
-        return true;
+
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == symbol && board[1][j] == symbol && board[2][j] == symbol)
+                return true;
+        }
+
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol)
+            return true;
+
+        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol)
+            return true;
+
+        return false;
     }
 }
